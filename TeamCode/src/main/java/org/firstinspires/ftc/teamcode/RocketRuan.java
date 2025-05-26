673ba62d969a4593240c9420baa3a9d140f085e7
Ruan <jesusmendes.zezinho@gmail.com>
2025-05-26 12:03:19 -0300

    Criação de movimentação "Começo"

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RocketRuan.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="RocketRuan", group="Linear Opmode")
+public class RocketRuan  extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FL0 = null;
+    private DcMotor FR1 = null ;
+    private DcMotor BL2 = null;
+    private DcMotor BR3 = null;
+
+    @Override
+    public void runOpMode() {
+
+    }
+
+}
